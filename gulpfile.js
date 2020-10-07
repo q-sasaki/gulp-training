b9155d26c5b57bc23bb6e9b2a0704fc2e044fb79
Yuki Sasaki <y-sasaki@q-co.jp>
2020-10-07 15:36:30 +0900

    RENEWAL-3868 Minify index.scss and rename .min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const gulp = require("gulp")
 const sass = require("gulp-sass")
+const cssmin = require("gulp-cssmin")
+const rename = require("gulp-rename")
 
 gulp.task("sass", function() {
   gulp.src("sass/**/*.scss")
     .pipe(sass({outputStyle: "expanded"}))
+    .pipe(cssmin())
+    .pipe(rename({suffix: ".min"}))
     .pipe(gulp.dest("./dist/css"))
 })
