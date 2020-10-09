a17e98226cf4aeb5caa53bba860b94c6f448c8fb
Yuki Sasaki <y-sasaki@q-co.jp>
2020-10-09 17:56:25 +0900

    RENEWAL-3875 Create GET /api/messages route which responds message list data in JSON.

--- express/app.js
+++ express/app.js
@@ -10,6 +10,17 @@ app.get("/messages", (req, res) => {
   res.send("hello sasaki!")
 })
 
+app.get("/api/messages", (req, res) => {
+  const messageLists = {
+    1: "aaa",
+    2: "bbb",
+    3: "ccc",
+    4: "ddd"
+  }
+
+  res.json(messageLists)
+})
+
 app.listen(port, () => {
   console.log(`At http://localhost:${port}`);
 })
