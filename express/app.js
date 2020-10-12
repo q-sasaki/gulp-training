2afe17f16e8bf97292004253140dfb581afac9cc
Yuki Sasaki <y-sasaki@q-co.jp>
2020-10-12 16:23:12 +0900

    RENEWAL-3875 Create POST /api/messages/<key> which edit the JSON data of 3. POSTed keys should be sent with expected key and one of expected option, deny it otherwise. Also authorize the request with a simple secret key saved on the server. It should be sent as a request header.

--- express/app.js
+++ express/app.js
@@ -2,13 +2,8 @@ const express = require("express")
 const app = express()
 const port = 8080
 
-app.get("/", (req, res) => {
-  res.send("hello express!")
-})
-
-app.get("/messages", (req, res) => {
-  res.send("hello sasaki!")
-})
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 const messageLists = {
   1: "aaa",
@@ -17,13 +12,13 @@ const messageLists = {
   4: "ddd"
 }
 
-// メッセージだけなら、配列だとよりシンプルかも → 配列の場合は-1する
-// const messageLists = [
-//   "aaa",
-//   "bbb",
-//   "ccc",
-//   "ddd"
-// ]
+app.get("/", (req, res) => {
+  res.send("hello express!")
+})
+
+app.get("/messages", (req, res) => {
+  res.send(messageLists)
+})
 
 app.get("/api/messages", (req, res) => {
   res.json(messageLists)
@@ -31,6 +26,26 @@ app.get("/api/messages", (req, res) => {
 
 app.get("/api/messages/:messageId", (req, res) => {
   res.send(messageLists[req.params.messageId])
+})
+
+app.post("/api/messages/:messageId", (req, res) => {
+  // リクエストヘッダのauth-keyが正しくなければ、要求を拒否
+  if ("sasaking" !== req.get("auth-key")) {
+    res.status(401).send("auth-key is incorrect")
+  }
+
+  const id = req.params.messageId
+  const message = req.body.message
+
+  // リクエストボディに keyには"message"、 valueにはメッセージ内容を含める。
+  // メッセージ内容が空の場合はエラー
+  if(message) {
+    const crud = messageLists[id] ? "edit" : "create"
+    messageLists[id] = message
+    res.send(`${crud} id: ${id} message ${message}`)
+  } else {
+    res.status(400).send(`your message is empty`)
+  }
 })
 
 app.listen(port, () => {
